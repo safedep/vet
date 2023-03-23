3db93e9726af46eacf2f75f64e24b6670aeb42c6
abhisek <abhisek.datta@gmail.com>
2023-03-23 16:34:08 +0530

    Publish events from Filter anlayzer module

--- pkg/analyzer/event.go
+++ pkg/analyzer/event.go
@@ -3,3 +3,7 @@ package analyzer
 func (ev *AnalyzerEvent) IsFailOnError() bool {
 	return ev.Type == ET_AnalyzerFailOnError
 }
+
+func (ev *AnalyzerEvent) IsFilterMatch() bool {
+	return ev.Type == ET_FilterExpressionMatched
+}
