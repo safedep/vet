31fa59b9e3c11d551e71872c2d67872788e083d2
abhisek <abhisek.datta@gmail.com>
2023-12-29 10:42:37 +0530

    test: Add E2E for LFP npm analyzer

--- test/scenarios/all.sh
+++ test/scenarios/all.sh
@@ -4,6 +4,7 @@ set -ex
 
 export E2E_THIS_DIR=$(dirname $0)
 export E2E_ROOT="$E2E_THIS_DIR/../../"
+export E2E_FIXTURES="$E2E_THIS_DIR/fixtures"
 export E2E_VET_BINARY="$E2E_ROOT/vet"
 
 $E2E_VET_BINARY auth configure --community
@@ -11,3 +12,4 @@ $E2E_VET_BINARY auth configure --community
 bash $E2E_THIS_DIR/scenario-1-vet-scans-vet.sh
 bash $E2E_THIS_DIR/scenario-2-vet-scan-demo-client-java.sh
 bash $E2E_THIS_DIR/scenario-3-filter-fail-fast.sh
+bash $E2E_THIS_DIR/scenario-4-lfp-fail-fast.sh
