ce7fff57cf88ac13d8600f25a8b2492b0c4cbb4a
abhisek <abhisek.datta@gmail.com>
2024-10-19 10:55:43 +0530

    test: Add skip github dep graph API for e2e test

--- test/scenarios/scenario-3-filter-fail-fast.sh
+++ test/scenarios/scenario-3-filter-fail-fast.sh
@@ -5,6 +5,7 @@ set -x
 $E2E_VET_BINARY scan -s --no-banner \
   --github https://github.com/safedep/demo-client-java.git \
   --report-summary=false \
+  --skip-github-dependency-graph-api \
   --filter 'vulns.critical.exists(p, p.id == "GHSA-4wrc-f8pq-fpqp")' \
   --filter-fail || exit 0
 
