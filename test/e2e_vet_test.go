aa1571031d7f6a2fd9dfea66f3fa8480737c8f8e
Manav Sethi <mail@manav.co.in>
2024-10-28 09:58:48 +0530

    Added judo for running cli tests

--- test/e2e_vet_test.go
+++ test/e2e_vet_test.go
@@ -1,18 +1,31 @@
 package test
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/rogpeppe/go-internal/testscript"
 )
 
 func TestE2EVet(t *testing.T) {
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("Failed to get current working directory: %v", err)
+	}
 	testscript.Run(t, testscript.Params{
-		Dir: "scenarios/scripts",
+		Dir:         "scripts/",
+		WorkdirRoot: wd,
 		Setup: func(env *testscript.Env) error {
-			// Set the environment variables
-			env.Setenv("E2E_VET_BINARY", "")
-			env.Setenv("E2E_ROOT", "")
+			thisDir := env.WorkDir
+			e2eRoot := filepath.Join(thisDir, "../../")
+			e2eFixtures := filepath.Join(thisDir, "/fixtures")
+			e2eVetBinary := filepath.Join(thisDir, "../vet")
+			// Set these values as environment variables in the test environment
+			env.Setenv("E2E_THIS_DIR", thisDir)
+			env.Setenv("E2E_ROOT", e2eRoot)
+			env.Setenv("E2E_FIXTURES", e2eFixtures)
+			env.Setenv("E2E_VET_BINARY", e2eVetBinary)
 			return nil
 		},
 	})
