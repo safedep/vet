2354b4af31987f6f83923e68f649d6103e836a4e
Abhisek Datta <abhisek.datta@gmail.com>
2025-01-22 11:20:51 +0530

    fix: Improve markdown summary report to make malware analysis section collapsable (#313)

--- pkg/reporter/markdown/emoji.go
+++ pkg/reporter/markdown/emoji.go
@@ -1,12 +1,13 @@
 package markdown
 
 const (
-	EmojiArrowRight     = ":arrow_right:"
-	EmojiWhiteCheckMark = ":white_check_mark:"
-	EmojiCrossMark      = ":x:"
-	EmojiLink           = ":link:"
-	EmojiWarning        = ":warning:"
-	EmojiRedExclamation = ":exclamation:"
-	EmojiOrangeCircle   = ":orange_circle:"
-	EmojiRedCircle      = ":red_circle:"
+	EmojiArrowRight        = ":arrow_right:"
+	EmojiWhiteCheckMark    = ":white_check_mark:"
+	EmojiCrossMark         = ":x:"
+	EmojiLink              = ":link:"
+	EmojiWarning           = ":warning:"
+	EmojiRedExclamation    = ":exclamation:"
+	EmojiOrangeCircle      = ":orange_circle:"
+	EmojiRedCircle         = ":red_circle:"
+	EmojiInformationSource = ":information_source:"
 )
