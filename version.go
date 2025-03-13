9bd1fb019bafe08a3ea921482e1c87253b29aeb1
Kunal Singh <kunalsin9h@gmail.com>
2025-03-13 20:54:10 +0530

    fix: missing version  on go install builds (#387)
    * fix: missing version information while building using go install
    
    * refactor: missing version on go install builds
    
    * refactor: go version set by both ldflags and go install(runtime)
    
    Signed-off-by: Kunal Singh <kunalsin9h@gmail.com>
    
    ---------
    
    Signed-off-by: Kunal Singh <kunalsin9h@gmail.com>

--- version.go
+++ version.go
@@ -3,12 +3,25 @@ package main
 import (
 	"fmt"
 	"os"
+	runtimeDebug "runtime/debug"
 
 	"github.com/spf13/cobra"
 )
 
+// When building with CI or Make, version is set using `ldflags`
 var version string
 var commit string
+
+func init() {
+	// Only use buildInfo if version wasn't set by ldflags, that is its being build by `go install`
+	if version == "" {
+		// Main.Version is based on the version control system tag or commit.
+		// This useful when app is build with `go install`
+		// See: https://antonz.org/go-1-24/#main-modules-version
+		var buildInfo, _ = runtimeDebug.ReadBuildInfo()
+		version = buildInfo.Main.Version
+	}
+}
 
 func newVersionCommand() *cobra.Command {
 	cmd := &cobra.Command{
