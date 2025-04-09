88f898f898835bb18334ad6e5cc35f6fec2d0ec3
Kunal Singh <kunalsin9h@gmail.com>
2025-04-09 10:48:15 +0530

    fix: error handing

--- pkg/common/packageregistry.go
+++ pkg/common/packageregistry.go
@@ -7,22 +7,24 @@ import (
 	"github.com/safedep/dry/packageregistry"
 )
 
+const errorScope = "Failed to resolve package latest version"
+
 // ResolvePackageLatestVersion resolves the latest version of a package
 // from the package registry
 func ResolvePackageLatestVersion(pkgName string, pkgEcosystem packagev1.Ecosystem) (string, error) {
 	pkg, err := packageregistry.NewRegistryAdapter(pkgEcosystem)
 	if err != nil {
-		return "", fmt.Errorf("failed to create package registry client: %v", err)
+		return "", fmt.Errorf("%s: failed to create package registry client: %v", errorScope, err)
 	}
 
 	pkgPacakgeDiscovery, err := pkg.PackageDiscovery()
 	if err != nil {
-		return "", fmt.Errorf("failed to discover package: %v", err)
+		return "", fmt.Errorf("%s: failed to discover package: %v", errorScope, err)
 	}
 
 	pkgData, err := pkgPacakgeDiscovery.GetPackage(pkgName)
 	if err != nil {
-		return "", fmt.Errorf("failed to get package data: %v", err)
+		return "", fmt.Errorf("%s: failed to get package data: %v", errorScope, err)
 	}
 
 	return pkgData.LatestVersion, nil
