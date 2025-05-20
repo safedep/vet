826b8eafdf436b907f149dd3a1fd3d9831eb5469
Kunal Singh <kunalsin9h@gmail.com>
2025-05-20 18:10:49 +0530

    Fix/ Bug in Resolving Package Version in Maven POM with Dependency Management (#487)
    * feat: pom.xml dependency resolver
    
    * feat: manifest from packages
    
    * feat: disable osv-scalibr's native loggin
    
    * fix: parser test, update total parsers count'
    
    * refactor: using manifest's NewPackageManifestFromLocal and AddPackage methods
    
    * fix: tests, package enumertaion with new pom.xml parser
    
    * fix: docker image with go 1.24.2
    
    * feat(test): pom parser
    
    * refactor: extracted commaon osv-scalibr's function in scalibr.go
    
    * refactor: setting scalibr logger in init function

---
+++ pkg/parser/pomxml.go
@@ -0,0 +1,71 @@
+package parser
+
+import (
+	"context"
+	"fmt"
+	scalibr "github.com/google/osv-scalibr"
+	el "github.com/google/osv-scalibr/extractor/filesystem/list"
+	"github.com/google/osv-scalibr/plugin"
+	"github.com/safedep/vet/pkg/common/logger"
+	"github.com/safedep/vet/pkg/models"
+)
+
+// parseMavenPomXmlFile parses the pom.xml file in a maven project.
+// Its finds the dependency from Maven Registry, and also from Parent Maven BOM
+// We use osc-scalibr's java/pomxmlnet (with Net, or Network) to fetch dependency from registry.
+func parseMavenPomXmlFile(lockfilePath string, _ *ParserConfig) (*models.PackageManifest, error) {
+	// Java/PomXMLNet extractor
+	ext, err := el.ExtractorsFromNames([]string{"java/pomxmlnet"})
+	if err != nil {
+		logger.Errorf("Failed to create java/pomxmlnet extractor form osv-scalibr: %s", err.Error())
+		return nil, fmt.Errorf("failed to create java/pomxmlnet extractor: %w", err)
+	}
+
+	// Capability is required for filtering the extractors,
+	// For example, osv-scalibr has 33 default extractors for instance, go, JavaScript, java/gradel, java/pomxml etc.
+	// Then this capability is used to filter with some property, like network (as required by our java/pomxmlnet)
+	capability := &plugin.Capabilities{
+		OS:            plugin.OSAny,
+		Network:       plugin.NetworkOnline,
+		DirectFS:      true,
+		RunningSystem: true,
+	}
+
+	// Apply capabilities
+	ext = el.FilterByCapabilities(ext, capability)
+
+	// Find the default scan root.
+	scanRoots, err := scanRoots()
+	if err != nil {
+		logger.Errorf("Failed to create scan roots for osv-scalibr: %s", err.Error())
+		return nil, fmt.Errorf("failed to create scan roots for osv-scalibr: %w", err)
+	}
+
+	// ScanConfig
+	config := &scalibr.ScanConfig{
+		ScanRoots:            scanRoots,
+		FilesystemExtractors: ext,
+		Capabilities:         capability,
+		PathsToExtract:       []string{lockfilePath},
+	}
+
+	result := scalibr.New().Scan(context.Background(), config)
+
+	if result.Status.Status != plugin.ScanStatusSucceeded {
+		logger.Warnf("osv-scalibr scan did not performed scan with success")
+		return nil, fmt.Errorf("osv-scalibr scan did not performed scan with success: Status %s", result.Status.String())
+	}
+
+	manifest := models.NewPackageManifestFromLocal(lockfilePath, models.EcosystemMaven)
+
+	for _, pkg := range result.Inventory.Packages {
+		pkgDetails := models.NewPackageDetail(models.EcosystemMaven, pkg.Name, pkg.Version)
+		modelPackage := &models.Package{
+			PackageDetails: pkgDetails,
+			Manifest:       manifest,
+		}
+		manifest.AddPackage(modelPackage)
+	}
+
+	return manifest, nil
+}
