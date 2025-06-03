14b24a086aec385eea059972822365ad84d44336
sachin maurya <sachin.maurya7666@gmail.com>
2025-06-03 11:18:12 +0530

    chore: removes dummy test
    Signed-off-by: sachin maurya <sachin.maurya7666@gmail.com>

--- pkg/parser/gomod_test.go
+++ pkg/parser/gomod_test.go
@@ -1,9 +1,6 @@
 package parser
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -26,44 +23,4 @@ func Test_GomodParser_Simple(t *testing.T) {
 	for _, pkg := range manifest.Packages {
 		assert.Contains(t, goDeps, pkg.Name)
 	}
-}
-
-func Test_GomodParser_Simple_NEW(t *testing.T) {
-	// allFiles := strings.Split("./tmp/vet-github-113631725", "/")
-	// modfile := allFiles[len(allFiles)-1]
-	// fmt.Printf("modfile: %v\n", modfile)
-	// newName := ""
-	// if modfile != "go.mod" {
-	// 	newName = strings.Join(allFiles[:len(allFiles)-1], "/")
-	// }
-	// finalName := newName
-
-	originalPath := "/tmp/vet-github-113631725"
-	newFileName := "renamed-file" // your desired new file name
-
-	// Extract directory path
-	dir := filepath.Dir(originalPath)
-	fmt.Printf("dir: %v\n", dir)
-
-	// Create new full path with the new file name
-	newPath := filepath.Join(dir, newFileName)
-
-	// Rename the file
-	err := os.Rename(originalPath, newPath)
-	if err != nil {
-		fmt.Printf("Error renaming file: %v\n", err)
-		return
-	}
-
-	fmt.Printf("File renamed from %s to %s\n", originalPath, newPath)
-
-	// manifest, err := parseGoModFile("./fixtures/go/go.mod", &ParserConfig{})
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-
-	// assert.Equal(t, 5, len(manifest.Packages))
-	// for _, pkg := range manifest.Packages {
-	// 	assert.Contains(t, goDeps, pkg.Name)
-	// }
 }
