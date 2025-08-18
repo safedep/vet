0f4c01b83a7cf380c3d9ab8eee0cc46491685117
Sahil Bansal <bansalsahil315@gmail.com>
2025-08-18 09:33:18 +0530

    add html reporter & create template for report (#559)
    * add html reporter & create template for report
    
    * updated table colors
    
    * chore: rm unused code block
    
    * add policy violations
    
    * chore: rm extra var
    
    * Update pkg/reporter/templates/report.templ
    
    Co-authored-by: Kunal Singh <kunalsin9h@gmail.com>
    Signed-off-by: Sahil Bansal <bansalsahil315@gmail.com>
    
    * chore: rm extra file
    
    * chore: rm unsued css property
    
    * add html reporter & create template for report
    
    * updated table colors
    
    * chore: rm unused code block
    
    * add policy violations
    
    * chore: rm extra var
    
    * Update pkg/reporter/templates/report.templ
    
    Co-authored-by: Kunal Singh <kunalsin9h@gmail.com>
    Signed-off-by: Sahil Bansal <bansalsahil315@gmail.com>
    
    * chore: rm extra file
    
    * chore: rm unsued css property
    
    * return error when failing to create html reporter
    
    ---------
    
    Signed-off-by: Sahil Bansal <bansalsahil315@gmail.com>
    Co-authored-by: Kunal Singh <kunalsin9h@gmail.com>

---
+++ pkg/reporter/templ.go
@@ -0,0 +1,29 @@
+package reporter
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"os"
+)
+
+// WriteTemplToFile renders a templ component to a file
+func WriteTemplToFile(component interface {
+	Render(ctx context.Context, w io.Writer) error
+}, path string) error {
+	// Create a buffer to hold the rendered template
+	buf := new(bytes.Buffer)
+
+	// Render the template to the buffer
+	if err := component.Render(context.Background(), buf); err != nil {
+		return fmt.Errorf("failed to render template: %w", err)
+	}
+
+	// Write the buffer contents to the file
+	if err := os.WriteFile(path, buf.Bytes(), 0644); err != nil {
+		return fmt.Errorf("failed to write to file %s: %w", path, err)
+	}
+
+	return nil
+}
