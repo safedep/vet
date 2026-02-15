3919182949304eecce723666c3f47eb202a9e0ec
Sergey Belash <again.against@gmail.com>
2026-02-15 13:51:44 +0100

    Added tests

--- pkg/analyzer/filter/eval_test.go
+++ pkg/analyzer/filter/eval_test.go
@@ -2,8 +2,10 @@ package filter
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	testing2 "k8s.io/utils/clock/testing"
 
 	"github.com/safedep/vet/gen/filtersuite"
 	"github.com/safedep/vet/gen/insightapi"
@@ -84,4 +86,52 @@ func TestEvaluatorLicenseExpression(t *testing.T) {
 			assert.Equal(t, c.expected, result.Matched())
 		})
 	}
+}
+
+func TestEvaluator_NowFunction(t *testing.T) {
+	pack := &models.Package{
+		PackageDetails: models.NewPackageDetail(models.EcosystemGo, "test2", "1.0.0"),
+		Manifest:       &models.PackageManifest{Ecosystem: models.EcosystemGo},
+	}
+	testClock := testing2.NewFakePassiveClock(
+		time.Date(2026, 2, 15, 13, 0, 0, 0, time.UTC),
+	)
+
+	t.Run("Function now() returns a timestamp", func(t *testing.T) {
+		eval, _ := NewEvaluator("test", WithClock(testClock))
+		filter := &filtersuite.Filter{
+			Name:  "test now",
+			Value: "now() == timestamp(\"2026-02-15T13:00:00Z\")",
+		}
+		err := eval.AddFilter(filter)
+		assert.NoError(t, err)
+
+		result, err := eval.EvalPackage(pack)
+		assert.NoError(t, err)
+		assert.Equal(t, true, result.Matched())
+	})
+
+	t.Run("Function now() can be converted to a duration", func(t *testing.T) {
+		eval, _ := NewEvaluator("test", WithClock(testClock))
+		filter := &filtersuite.Filter{
+			Name:  "test now",
+			Value: "(now() - timestamp(\"2026-02-15T10:00:00Z\")).getHours() == 3",
+		}
+		err := eval.AddFilter(filter)
+		assert.NoError(t, err)
+
+		result, err := eval.EvalPackage(pack)
+		assert.NoError(t, err)
+		assert.Equal(t, true, result.Matched())
+	})
+
+	t.Run("Now is a function", func(t *testing.T) {
+		eval, _ := NewEvaluator("test", WithClock(testClock))
+		filter := &filtersuite.Filter{
+			Name:  "test now",
+			Value: "now == timestamp(\"2026-02-15T10:00:00Z\")",
+		}
+		err := eval.AddFilter(filter)
+		assert.Error(t, err, "ERROR: <input>:1:1: undeclared reference to 'now'")
+	})
 }
