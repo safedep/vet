7547b3a298335208cf4ac26fb08c3618f993f099
Carla232 <79741526+Carla232@users.noreply.github.com>
2026-04-27 06:05:21 +0300

    test(reporter): fix markdown summary import grouping
    Signed-off-by: Carla232 <79741526+Carla232@users.noreply.github.com>

--- pkg/reporter/markdown_summary_test.go
+++ pkg/reporter/markdown_summary_test.go
@@ -4,10 +4,10 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/safedep/dry/reporting/markdown"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/safedep/dry/reporting/markdown"
 	jsonreportspec "github.com/safedep/vet/gen/jsonreport"
 )
 
